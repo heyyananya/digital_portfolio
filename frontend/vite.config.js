e25fd56bee74773924d265ca319a4847173039f3
ananyatech2006 <andypatel2406@gmail.com>
2026-07-11 09:47:48 +0530

    Configure base path in vite.config.js for default free GitHub Pages URL and remove CNAME

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/digital_portfolio_ananya/',
   plugins: [react()],
   server: {
     host: true,
