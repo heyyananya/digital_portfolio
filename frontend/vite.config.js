f703ab35e500aa066c3e809c2ef098bf824d139f
ananyatech2006 <andypatel2406@gmail.com>
2026-07-11 09:51:23 +0530

    Configure base path to relative './' to support dynamic repository renames

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/digital_portfolio_ananya/',
+  base: './',
   plugins: [react()],
   server: {
     host: true,
